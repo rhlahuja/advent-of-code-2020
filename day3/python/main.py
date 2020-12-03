a174eb440c1b761d01ca0094c0a41199b3c4a8c1
Rahul Ahuja <rah.ahuja@gmail.com>
2020-12-03 00:37:15 -0600

    Add Python + Rust solutions for Day 3

---
+++ day3/python/main.py
@@ -0,0 +1,38 @@
+import pathlib
+
+
+with open(pathlib.Path(__file__).parent.parent / 'input.txt') as f:
+    tree_map = f.read().splitlines()
+
+
+def num_trees_encountered(tree_map: list[str], slope: tuple[int, int]) -> int:
+    num_cols = len(tree_map[0])
+    num_rows = len(tree_map)
+
+    col_idx = 0
+    row_idx = 0
+
+    trees_encountered = 0
+
+    while row_idx < num_rows:
+        if tree_map[row_idx][col_idx] == '#':
+            trees_encountered += 1
+
+        col_idx += slope[0]
+        row_idx += slope[1]
+
+        if col_idx >= num_cols:
+            col_idx -= num_cols
+
+    return trees_encountered
+
+
+print('Part One: ', num_trees_encountered(tree_map, (3, 1)))
+print(
+    'Part Two: ',
+    num_trees_encountered(tree_map, (1, 1))
+    * num_trees_encountered(tree_map, (3, 1))
+    * num_trees_encountered(tree_map, (5, 1))
+    * num_trees_encountered(tree_map, (7, 1))
+    * num_trees_encountered(tree_map, (1, 2)),
+)
---
+++ day3/rust/src/main.rs
@@ -0,0 +1,47 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn num_trees_encountered(tree_map: &Vec<&str>, slope: (usize, usize)) -> u32 {
+    let num_cols = tree_map[0].chars().count();
+    let num_rows = tree_map.len();
+
+    let mut col_idx: usize = 0;
+    let mut row_idx: usize = 0;
+
+    let mut trees_encountered: u32 = 0;
+
+    while row_idx < num_rows {
+        if tree_map[row_idx].chars().nth(col_idx).unwrap() == "#".chars().next().unwrap() {
+            trees_encountered += 1;
+        }
+
+        col_idx += &slope.0;
+        row_idx += &slope.1;
+
+        if col_idx >= num_cols {
+            col_idx -= num_cols
+        }
+    }
+
+    trees_encountered
+}
+
+fn main() {
+    let input_filepath = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .join("input.txt");
+    let input = fs::read_to_string(input_filepath).unwrap();
+    let tree_map: Vec<&str> = input.lines().collect();
+
+    println!("Part One: {}", num_trees_encountered(&tree_map, (3, 1)));
+    println!(
+        "Part One: {}",
+        num_trees_encountered(&tree_map, (1, 1))
+            * num_trees_encountered(&tree_map, (3, 1))
+            * num_trees_encountered(&tree_map, (5, 1))
+            * num_trees_encountered(&tree_map, (7, 1))
+            * num_trees_encountered(&tree_map, (1, 2))
+    );
+}
