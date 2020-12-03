fd74f5d5565b1f314537f862fefff8342fc7049b
Rahul Ahuja <rah.ahuja@gmail.com>
2020-12-03 11:16:00 -0600

    Add Rust itertools solution

--- day-01/rust/src/main.rs
+++ day-01/rust/src/main.rs
@@ -1,9 +1,10 @@
-use std::path::Path;
-
 use std::env;
 use std::fs;
+use std::path::Path;
 
-fn part_one(numbers: &[i32]) -> i32 {
+use itertools::Itertools;
+
+fn part_one_naive(numbers: &[i32]) -> i32 {
     for num1 in numbers {
         let num2 = 2020 - num1;
         if numbers.contains(&num2) {
@@ -13,13 +14,22 @@ fn part_one(numbers: &[i32]) -> i32 {
     0
 }
 
-fn part_two(numbers: &[i32]) -> i32 {
+fn part_two_naive(numbers: &[i32]) -> i32 {
     for num1 in numbers {
         for num2 in numbers {
             let num3 = 2020 - num1 - num2;
             if numbers.contains(&num3) {
                 return num1 * num2 * num3;
             }
+        }
+    }
+    0
+}
+
+fn itertools_combination_product(numbers: &[i32], num_elements: usize) -> i32 {
+    for expenses in numbers.iter().combinations(num_elements) {
+        if expenses.clone().into_iter().sum::<i32>() == 2020i32 {
+            return expenses.into_iter().product::<i32>();
         }
     }
     0
@@ -37,6 +47,18 @@ fn main() {
         vec.push(line.parse::<i32>().unwrap());
     }
 
-    println!("Part One: {}", part_one(vec.as_slice()));
-    println!("Part Two: {}", part_two(vec.as_slice()));
+    let part_one_naive_solution = part_one_naive(vec.as_slice());
+    let part_two_naive_solution = part_two_naive(vec.as_slice());
+
+    println!("Part One: {}", part_one_naive_solution);
+    println!("Part Two: {}", part_two_naive_solution);
+
+    assert_eq!(
+        part_one_naive_solution,
+        itertools_combination_product(&vec, 2)
+    );
+    assert_eq!(
+        part_two_naive_solution,
+        itertools_combination_product(&vec, 3)
+    );
 }
