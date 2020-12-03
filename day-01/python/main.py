738b29af7c6f89f5d1a2e65368585fdf6709cbfa
Rahul Ahuja <rah.ahuja@gmail.com>
2020-12-03 10:31:47 -0600

    Add itertools solution

--- day-01/python/main.py
+++ day-01/python/main.py
@@ -1,14 +1,16 @@
+import math
 import pathlib
+import itertools
 
 
-def part_one(numbers: list[int]) -> int:
+def part_one_naive(numbers: list[int]) -> int:
     for num1 in numbers:
         num2 = 2020 - num1
         if num2 in numbers:
             return num1 * num2
 
 
-def part_two(numbers: list[int]) -> int:
+def part_two_naive(numbers: list[int]) -> int:
     for num1 in numbers:
         for num2 in numbers:
             num3 = 2020 - num1 - num2
@@ -16,8 +18,21 @@ def part_two(numbers: list[int]) -> int:
                 return num1 * num2 * num3
 
 
+def itertools_combination_product(numbers: list[int], num_elements: int) -> int:
+    expenses = [
+        e for e in itertools.combinations(numbers, num_elements) if sum(e) == 2020
+    ][0]
+    return math.prod(expenses)
+
+
 with open(pathlib.Path(__file__).parent.parent / 'input.txt') as f:
     numbers = [int(line) for line in f]
 
-print('Part One: ', part_one(numbers))
-print('Part Two: ', part_two(numbers))
+part_one_naive_solution = part_one_naive(numbers)
+part_two_naive_solution = part_two_naive(numbers)
+
+print('Part One: ', part_one_naive(numbers))
+print('Part Two: ', part_two_naive(numbers))
+
+assert part_one_naive_solution == itertools_combination_product(numbers, 2)
+assert part_two_naive_solution == itertools_combination_product(numbers, 3)
