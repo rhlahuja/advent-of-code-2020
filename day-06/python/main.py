9294b62a0e5cab6e88e054b805282f1829db75e1
Rahul Ahuja <rah.ahuja@gmail.com>
2020-12-06 12:37:43 -0600

    Add Day 5 Python list comp solution

--- day-06/python/main.py
+++ day-06/python/main.py
@@ -17,8 +17,24 @@ def part_two(group_answers: list[str]) -> int:
     return sum_all_yes_questions
 
 
+def part_two_list_comp(group_answers: list[str]) -> int:
+    return sum(
+        sum(
+            bool(k)
+            for k, v in sum(
+                (Counter(answer) for answer in individual_answers), Counter()
+            ).items()
+            if v == len(individual_answers)
+        )
+        for individual_answers in (g.splitlines() for g in group_answers)
+    )
+
+
 with open(pathlib.Path(__file__).parent.parent / 'input.txt') as f:
     group_answers = f.read().split('\n\n')
 
 print('Part One:', part_one(group_answers))
-print('Part Two:', part_two(group_answers))
+
+part_two_solution = part_two(group_answers)
+assert part_two_solution == part_two_list_comp(group_answers)
+print('Part Two:', part_two_solution)
