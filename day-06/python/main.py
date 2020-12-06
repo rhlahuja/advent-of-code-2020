6f042cae54ea869510d29a2fe62379c4f2484226
Rahul Ahuja <rah.ahuja@gmail.com>
2020-12-06 02:26:00 -0600

    Encapsulate part logic

--- day-06/python/main.py
+++ day-06/python/main.py
@@ -2,19 +2,23 @@ import pathlib
 from collections import Counter
 
 
+def part_one(group_answers: list[str]) -> int:
+    return sum(len(set(answers.replace('\n', ''))) for answers in group_answers)
+
+
+def part_two(group_answers: list[str]) -> int:
+    sum_all_yes_questions = 0
+    for answers in group_answers:
+        individual_answers = answers.splitlines()
+        counts = sum((Counter(answer) for answer in individual_answers), Counter())
+        sum_all_yes_questions += sum(
+            bool(k) for k, v in counts.items() if v == len(individual_answers)
+        )
+    return sum_all_yes_questions
+
+
 with open(pathlib.Path(__file__).parent.parent / 'input.txt') as f:
     group_answers = f.read().split('\n\n')
 
-
-print(
-    'Part One:', sum(len(set(answers.replace('\n', ''))) for answers in group_answers)
-)
-
-sum_all_yes_questions = 0
-for answers in group_answers:
-    individual_answers = answers.splitlines()
-    counts = sum((Counter(answer) for answer in individual_answers), Counter())
-    sum_all_yes_questions += sum(
-        bool(k) for k, v in counts.items() if v == len(individual_answers)
-    )
-print('Part Two:', sum_all_yes_questions)
+print('Part One:', part_one(group_answers))
+print('Part Two:', part_two(group_answers))
