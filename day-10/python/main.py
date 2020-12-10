a9a718b781ce81cab5e9acf6cbedb57af0b5cf6b
Rahul Ahuja <rah.ahuja@gmail.com>
2020-12-10 14:20:23 -0600

    Improve naming

--- day-10/python/main.py
+++ day-10/python/main.py
@@ -2,30 +2,31 @@ import pathlib
 from collections import defaultdict
 
 
-def find_joltage_differences_product(adapter_jolts: list[int]) -> int:
+def find_joltage_differences_product(adapters: list[int]) -> int:
     jolt_differences = defaultdict(int)
-    for index, jolts in enumerate(adapter_jolts[:-1]):
-        jolt_differences[adapter_jolts[index + 1] - jolts] += 1
+    for index, adapter_jolts in enumerate(adapters[:-1]):
+        jolt_differences[adapters[index + 1] - adapter_jolts] += 1
     return jolt_differences[1] * jolt_differences[3]
 
 
-def find_valid_permutations(adapter_jolts: list[int]) -> int:
+def find_num_valid_permutations(adapters: list[int]) -> int:
     num_valid_permutations = {0: 1}
-    for jolts in adapter_jolts[1:]:
-        num_valid_permutations[jolts] = sum(
-            num_valid_permutations.get(jolts - delta, 0) for delta in range(1, 4)
+    for adapter_jolts in adapters[1:]:
+        num_valid_permutations[adapter_jolts] = sum(
+            num_valid_permutations.get(adapter_jolts - delta, 0)
+            for delta in range(1, 4)
         )
-    return num_valid_permutations[adapter_jolts[-1]]
+    return num_valid_permutations[adapters[-1]]
 
 
 with open(pathlib.Path(__file__).parent.parent / 'input.txt') as f:
-    adapter_jolts = sorted(int(line) for line in f.read().splitlines())
+    adapters = sorted(int(line) for line in f.read().splitlines())
 
-adapter_jolts.insert(0, 0)
-adapter_jolts.append(adapter_jolts[-1] + 3)
+adapters.insert(0, 0)
+adapters.append(adapters[-1] + 3)
 
-part_one_solution = find_joltage_differences_product(adapter_jolts)
-part_two_solution = find_valid_permutations(adapter_jolts)
+part_one_solution = find_joltage_differences_product(adapters)
+part_two_solution = find_num_valid_permutations(adapters)
 
 print('Part One:', part_one_solution)
 print('Part Two:', part_two_solution)
--- day-10/rust/src/main.rs
+++ day-10/rust/src/main.rs
@@ -2,31 +2,35 @@ use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 
-fn find_jolt_differences_product(adapter_jolts: &[i64]) -> i64 {
+fn find_jolt_differences_product(adapters: &[i64]) -> i64 {
     let mut jolt_differences = HashMap::new();
-    for (index, jolts) in adapter_jolts[..adapter_jolts.len() - 1].iter().enumerate() {
+    for (index, adapter_jolts) in adapters[..adapters.len() - 1].iter().enumerate() {
         *jolt_differences
-            .entry(adapter_jolts[index + 1] - jolts)
+            .entry(adapters[index + 1] - adapter_jolts)
             .or_insert(0) += 1;
     }
     jolt_differences[&1] * jolt_differences[&3]
 }
 
-fn find_valid_permutations(adapter_jolts: &[i64]) -> i64 {
+fn find_num_valid_permutations(adapters: &[i64]) -> i64 {
     let mut num_valid_permutations: HashMap<_, _> = [(0, 1)].iter().cloned().collect();
-    for jolts in adapter_jolts[1..].iter() {
+    for adapter_jolts in adapters[1..].iter() {
         num_valid_permutations.insert(
-            *jolts,
+            *adapter_jolts,
             (1..4)
-                .map(|delta| num_valid_permutations.get(&(jolts - delta)).unwrap_or(&0))
+                .map(|delta| {
+                    num_valid_permutations
+                        .get(&(adapter_jolts - delta))
+                        .unwrap_or(&0)
+                })
                 .sum(),
         );
     }
-    num_valid_permutations[&adapter_jolts.last().unwrap()]
+    num_valid_permutations[&adapters.last().unwrap()]
 }
 
 fn main() {
-    let mut adapter_jolts: Vec<_> = fs::read_to_string(
+    let mut adapters: Vec<_> = fs::read_to_string(
         Path::new(env!("CARGO_MANIFEST_DIR"))
             .parent()
             .unwrap()
@@ -37,12 +41,12 @@ fn main() {
     .map(|l| l.parse().unwrap())
     .collect();
 
-    adapter_jolts.sort_unstable();
-    adapter_jolts.insert(0, 0);
-    adapter_jolts.push(adapter_jolts.last().unwrap() + 3);
+    adapters.sort_unstable();
+    adapters.insert(0, 0);
+    adapters.push(adapters.last().unwrap() + 3);
 
-    let part_one_solution = find_jolt_differences_product(&adapter_jolts);
-    let part_two_solution = find_valid_permutations(&adapter_jolts);
+    let part_one_solution = find_jolt_differences_product(&adapters);
+    let part_two_solution = find_num_valid_permutations(&adapters);
 
     println!("Part One: {}", &part_one_solution);
     println!("Part Two: {}", &part_two_solution);
